fa4abf9b8dfb787a59c7461eb0b661c96da8f0e2
kiwoon Kim <87232579+tigerenergy@users.noreply.github.com>
2021-07-26 20:48:11 +0900

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,63 @@
+  
+import './style.css'
+import * as THREE from 'three'
+
+
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+
+
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000})
+)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00})
+  )
+  cube2.position.set(-2, 0, 0)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff})
+)
+  cube3.position.set(2, 0, 0)
+group.add(cube1, cube2, cube3)
+
+
+// Axes helper
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas.webgl')
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
